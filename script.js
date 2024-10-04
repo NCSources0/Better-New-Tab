ead9de8b03985bcaa35bb875f96644f62d2c1d89
NCSources <ncsources0@gmail.com>
2024-10-04 17:43:23 -0400

    😒

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ location.hash = '';
 const searchBar = document.getElementsByClassName('searchBar')[0],
   search = location.search.split('=')[1],
   iframe = document.getElementsByClassName('page')[0],
-  topBar = document.getElementsByClassName('topBar')[0];
+  topBar = document.getElementsByClassName('topBar')[0],
+  localStoragePopup = document.getElementsByClassName('localStorage')[0];
 
 iframe.setAttribute('src', 'search.html#gsc.q=');
 if (location.search != '') {
@@ -26,7 +27,26 @@ for (let i = 0; i < search.length; i++) {
 }
 
 setInterval(() => {
-  document.querySelectorAll('a').forEach((a) => {
-    a.setAttribute('target', '_blank');
-  })
+  if (!localStorage.getItem('consent')) {
+    localStoragePopup.style.bottom = '0';
+  } else {
+    localStoragePopup.style.bottom = '';
+  }
 }, 0)
+
+function bookmark() {
+  const name = prompt('Bookmark name:');
+}
+
+function ask(what) {
+  let promptBG = document.createElement('div'),
+    prompt = document.createElement('div'),
+    promptInput = document.createElement('input');
+  promptBG.setAttribute('class', 'promptBG');
+  prompt.setAttribute('class', 'prompt');
+  promptInput.setAttribute('class', 'promptInput');
+  prompt.innerHTML = `${what}:`;
+  prompt.appendChild(promptInput);
+  promptBG.appendChild(prompt);
+  document.body.appendChild(promptBG);
+}
